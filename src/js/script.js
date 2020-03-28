8f1b7890ed037e486197c7fe199d7cabe0a21f09
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-03-28 20:52:18 +0100

    Add initAccordion method

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,7 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
       console.log('new Product:', thisProduct);
     }
@@ -83,6 +84,54 @@
 
       menuContainer.appendChild(thisProduct.element);
     }
+
+    initAccordion(){
+        const thisProduct = this;
+
+        /* find the clickable trigger (the element that should react to clicking) */
+
+        const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+        /* START: click event listener to trigger */
+
+        clickableTrigger.addEventListener('click', function(event){
+
+        /* prevent default action for event */
+
+        event.preventDefault();
+
+        /* toggle active class on element of thisProduct */
+
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+
+        /* find all active products */
+
+        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
+
+        /* START LOOP: for each active product */
+
+        for (let activeProduct of activeProducts) {
+
+          /* START: if the active product isn't the element of thisProduct */
+
+          if (!(activeProduct === thisProduct.element)) {
+
+            /* remove class active for the active product */
+
+            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+
+          /* END: if the active product isn't the element of thisProduct */
+
+          }
+
+        /* END LOOP: for each active product */
+
+        }
+
+      /* END: click event listener to trigger */
+      });
+    }
+
   }
 
   const app = {
