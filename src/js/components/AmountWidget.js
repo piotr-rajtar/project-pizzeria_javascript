b97be673bdb84384215954e5a90321cc6d25d3dc
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-04-12 20:52:52 +0200

    Restore project and remove comments

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -2,7 +2,7 @@ import {settings, select} from '../settings.js';
 
 class AmountWidget {
   constructor(element) { //tu przekazuje kod html
-    const thisWidget = this; //na podstawie kodu tworzy sie obiekt, z guzikiami i elementami HTML
+    const thisWidget = this;
 
     thisWidget.getElements(element); //wywoluje metode getElements ktora wyodrebniam guziki i miejsce na liczbe
 
@@ -10,6 +10,7 @@ class AmountWidget {
 
     thisWidget.setValue(thisWidget.input.value); //uruchamiam metode set value ktora wstawia mi nowe liczby na strone
     thisWidget.initActions(thisWidget.input.value);
+   
   }
 
   getElements(element){
@@ -41,7 +42,7 @@ class AmountWidget {
   initActions() {
     const thisWidget = this;
 
-    const inputValue = thisWidget.input; //const czy let
+    const inputValue = thisWidget.input;
     const plusButton = thisWidget.linkIncrease;
     const minusButton = thisWidget.linkDecrease;
 
