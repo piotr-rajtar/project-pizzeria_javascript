b12ce4ff7d39a2c12c52228436f6fb0ab0d68364
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-04-13 13:53:53 +0200

    Add initBooking method

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function(){
@@ -105,6 +106,12 @@ const app = {
     });
   },
 
+  initBooking: function(){
+    const thisApp = this;
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingContainer);
+  },
+
   init: function(){
     const thisApp = this;
 
@@ -112,6 +119,7 @@ const app = {
 
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
