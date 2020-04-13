cb6e034350699182ea2bffc8735c1ae0cbe3a0ff
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-04-13 13:31:53 +0200

    Reorganize project + add new methods in app object

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,68 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', ''); //wydobywanie hasha z adresu strony
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        event.preventDefault();
+        const clickedElement = this;
+
+        /* get ID from gref attribut */
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run activate page with that ID */
+
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching page, remove from non-matching */
+
+    for (let page of thisApp.pages){
+      page.classList.toggle(
+        classNames.pages.active,
+        page.id == pageId
+      );
+    }
+
+    /* add class "active" to matching link, remove from non-matching */
+
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href').replace('#', '') == pageId
+      );
+    }
+  },
+
   initMenu: function(){
     const thisApp = this;
 
@@ -47,6 +107,8 @@ const app = {
 
   init: function(){
     const thisApp = this;
+
+    thisApp.initPages();
 
     thisApp.initData();
     thisApp.initCart();
