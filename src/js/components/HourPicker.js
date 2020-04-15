388c99c65b03d6c04470745a465e94c412d3e5d4
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-04-16 00:09:50 +0200

    Add constructor in HourPicker class

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,6 +1,17 @@
+import {select, settings} from '../settings.js';
 import BaseWidget from './BaseWidget.js';
 
 class HourPicker extends BaseWidget {
+  constructor(wrapper) {
+    super(wrapper, settings.hours.open);
+
+    const thisWidget = this;
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
+    thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+
+    thisWidget.initPlugin();
+  }
 
 }
 
