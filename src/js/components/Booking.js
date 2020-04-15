b4f88f798cc4107e88aba705a5345a4c4d7eb3be
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-04-15 18:50:50 +0200

    Import DatePicker class

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,6 @@
 import {templates, select} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
 
 class Booking {
   constructor(element){
