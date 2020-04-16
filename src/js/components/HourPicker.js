a16f4ce5b49efb4f46550cb924afebe1aa397eee
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-04-16 17:06:28 +0200

    Update HourPicker class

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,3 +1,6 @@
+/* global rangeSlider */
+
+import {utils} from '../utils.js';
 import {select, settings} from '../settings.js';
 import BaseWidget from './BaseWidget.js';
 
@@ -11,6 +14,33 @@ class HourPicker extends BaseWidget {
     thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
 
     thisWidget.initPlugin();
+
+    thisWidget.value = thisWidget.dom.input.value;
+  }
+
+  initPlugin() {
+    const thisWidget = this;
+
+    rangeSlider.create(thisWidget.dom.input);
+
+    thisWidget.dom.input.addEventListener('input', function(){
+      thisWidget.value = thisWidget.dom.input.value;
+    });
+
+  }
+
+  parseValue(value) {
+    return utils.numberToHour(value);
+  }
+
+  isValid() {
+    return true;
+  }
+
+  renderValue() {
+    const thisWidget = this;
+
+    thisWidget.dom.output.innerHTML = thisWidget.value;
   }
 
 }
